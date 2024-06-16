6cfd9bf633c878039ea3b51906d94d20b3c65f95
meglizcode <meglizjudd@gmail.com>
2024-06-16 17:50:30 -0500

    Task 4: WIP Tests not running correctly

--- TechJobs.Test/JobTests.cs
+++ TechJobs.Test/JobTests.cs
@@ -6,11 +6,19 @@ namespace TechJobs.Tests
 	{
         //Testing Objects
         //initalize your testing objects here
+//Testing objects
+Job job1 = new Job();
+
+Job job2 = new Job();
+
+Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+Job job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
 
         [TestMethod]
-        public void TestMethod()
+        public void TestSettingJobId()
         {
-            //TODO: Task 4: remove this method before you create your first test method 
+            Assert.IsFalse(job1.Id == job2.Id); 
         }
     }
 }
